33c994a80db3eed72355d554cc5e93c1662e0abe
Yuss3f <kayke8youssef@gmail.com>
2023-11-14 06:12:48 -0800

    part 1

---
+++ _printf.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ *
+ * Return: Number of characters printed (excluding null byte)
+ */
+int _printf(const char *format, ...);
+{
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
+	params_t params = PARAMS_INIT;
+
+	va_start(ap, format);
+
+	if(!format || (format[0] == '%' && !format[2])
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
+	{
+		init_params(&params, ap);
+		if (*p != '%')
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/*_printf.c module*/
+
+int _printf(const char *format, ...);
+
+#endif
